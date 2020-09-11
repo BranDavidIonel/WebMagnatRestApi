5258e3058baea2e45182e1e2f09ebdae6903b082
Bran David Ionel <bran.david96@yahoo.com>
2020-09-11 23:04:04 +0300

    I changed the query for gruped the status, I add the procentage

--- app/Http/Controllers/WebMagnatController.php
+++ app/Http/Controllers/WebMagnatController.php
@@ -8,17 +8,20 @@ use App\Models\WebMagnatModel;
 class WebMagnatController extends Controller
 {
     public function index(){
+        //the 200 means 'ok' it is a cod result
+        //the response is for api in json format
         return response()->json(WebMagnatModel::get(),200);
     }
     //get all data from table web_magnat_info ( not in group by service_status)
     public function statuses(){
         return response()->json(WebMagnatModel::get(),200);
     }
-
+    
     public function groupedStatus(){
 
             //response()->json(DB::select('SELECT web_magnat_info.service_status, COUNT(web_magnat_info.service_status) as nrStatuses FROM web_magnat_info GROUP BY web_magnat_info.service_status'));
             $webMagnatModel= new WebMagnatModel();
+            //a made a method in Model 'WebMagnatModel'
             $date=$webMagnatModel->readGroupedStatus();
             //print_r($date);
             return  response()->json($date,200);
--- app/Models/WebMagnatModel.php
+++ app/Models/WebMagnatModel.php
@@ -17,9 +17,14 @@ class WebMagnatModel extends Model
     ];
     public function readGroupedStatus(){
         //return DB::connection('main')->select('SELECT web_magnat_info.service_status, COUNT(web_magnat_info.service_status) as nrStatuses FROM web_magnat_info GROUP BY web_magnat_info.service_status');
-        return DB::select('SELECT web_magnat_info.service_status, 
+        return DB::select('SELECT 
+        web_magnat_info.service_status,
+        t1.nrSum,
+        ((COUNT(web_magnat_info.service_status)*100)/t1.nrSum) as percentage,
         COUNT(web_magnat_info.service_status) as nrStatuses 
-        FROM web_magnat_info 
-        GROUP BY web_magnat_info.service_status');
+         FROM web_magnat_info 
+         INNER JOIN (SELECT COUNT(*) as nrSum FROM  web_magnat_info) as t1 
+         ON 1=1
+         GROUP BY web_magnat_info.service_status,t1.nrSum');
     }
 }
